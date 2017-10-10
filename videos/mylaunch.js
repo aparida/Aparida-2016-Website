9baec6ce4afbd346e09600bb1e0370687ac5b51c
Avinash <avipar@me.com>
2017-10-09 17:16:53 -0700

    Add files via upload

---
+++ videos/mylaunch.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+
+    // index.html
+     
+   $(".rect-3").addClass("wow animated fadeInUpShort").css({
+        'animation-name':'fadeInUpShort',
+        'animation-duration':'1000ms',
+        'animation-delay':'0.4s',
+   });
+
+});
+
+
+
+
+
